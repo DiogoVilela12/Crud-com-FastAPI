4318cd8c003be28d4f0ff1ab42eb28dde6bead6f
Diogo Vilela <90631238+DiogoVilela12@users.noreply.github.com>
2022-01-20 07:29:20 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from typing import Optional
-from Utils import validação
-from Utils import arquivo
 import sqlite3
 
 app = FastAPI()
@@ -62,3 +60,60 @@ async def modificar_usuario(id_atual: int, nome: Optional[str], cpf: Optional[in
     conn.close()
 
     return {f'Dados do Usuario(a): {nome} atualizados com sucesso !!!'}
+
+
+@app.get("/Deletar usuario")
+async def deletar_usuario(id_do_usuario: int):
+
+    conn = sqlite3.connect('Banco De Dados/banco_de_dados.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    DELETE FROM User
+    WHERE id = ?
+    """, (id_do_usuario,))
+
+    conn.commit()
+    conn.close()
+    return {f'Você deletou o usuario com o id: {id_do_usuario}'}
+
+
+@app.get("/Buscar usuario")
+async def buscar_usuario_id(id_do_usuario: int):
+
+    conn = sqlite3.connect('Banco De Dados/banco_de_dados.db')
+    cursor = conn.cursor()
+    resultado = []
+
+    cursor.execute("""
+    SELECT nome, idade, cpf FROM User
+    WHERE id = ?
+    """, (id_do_usuario,))
+
+    for dado in cursor.fetchall():
+        resultado.append(dado)
+
+    conn.close()
+    return {f'Resultado encontrado {resultado}'}
+
+
+@app.get("/Buscar nome")
+async def buscar_usuario_nome(nome_do_usuario: str):
+
+    conn = sqlite3.connect('Banco De Dados/banco_de_dados.db')
+    cursor = conn.cursor()
+    resultado = []
+
+    cursor.execute("""
+    SELECT nome, idade, cpf FROM User
+    WHERE nome = ?
+    """, (nome_do_usuario,))
+
+    for dado in cursor.fetchall():
+        resultado.append(dado)
+
+    conn.close()
+    if len(resultado)  > 0:
+        return {f'Resultado encontrado {resultado}'}
+    else:
+        return {f'Resultado não encontrado DIGITE EXATAMANTE SEU NOME {resultado}'}
